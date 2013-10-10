1c56faa0d83b14587844a73a4faad0d4d54a9ff6
Bob Heise <rheise@coredial.com>
2013-10-10 13:42:08 -0400

    bump package version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="suds",
-    version=suds.__version__,
+    version=suds.__version__ + "-tz",
     description="Lightweight SOAP client",
     author="Jeff Ortel",
     author_email="jortel@redhat.com",
